c2e5eed151d41b0bf024c65c8f0786fbff73df9b
Sail <151263555@qq.com>
2023-06-24 19:37:15 +0800

    安装程序

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +12,84 @@ namespace webprotocol
 {
     internal class Program
     {
+        private readonly static Log log = new Log();
 
         static void Main(string[] args)
         {
-            Log log = new Log();
-            log.Write(args);
+            string url = "http://127.0.0.1:4000/a.docx";
+            string fileType = "word";
+            bool preview = true;
+            // string tempFile = Downloadfile(url);
+            string tempFile = "C:\\Users\\15126\\Desktop\\a.docx";
+            log.Write("tempFile:" + tempFile);
+            if (fileType == "word")
+            {
+                PrintWord(tempFile, preview);
+            }
+            else if (fileType == "excel")
+            {
+
+            }
+        }
+
+        private static string Downloadfile(string url)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            WebClient client = new WebClient();
+            string tempFile = Path.GetTempFileName();
+            client.DownloadFile(url, tempFile);
+            return tempFile;
+        }
+
+        private static void PrintWord(string fileName, bool preview)
+        {
+            try
+            {
+                var word = new Microsoft.Office.Interop.Word.Application { Visible = false }; // 启动Word进程
+                var doc = word.Documents.Open(fileName, ReadOnly: true, Visible: true); // 打开待打印的文档
+                doc.PrintOut(); // 打印
+                doc.Close(SaveChanges: false); // 关闭文档
+                // word.Quit(SaveChanges: false); // 退出Word进程
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(word);
+            }
+            catch(Exception ex)
+            {
+                log.Write(ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private static void PrintExcel(string fileName, bool preview)
+        {
+            Microsoft.Office.Interop.Excel.Application xApp = new Microsoft.Office.Interop.Excel.Application
+            {
+                Visible = false
+            };
+            Microsoft.Office.Interop.Excel.Workbook xBook = xApp.Workbooks._Open(fileName);
+            Microsoft.Office.Interop.Excel.Worksheet xSheet = null;
+            try
+            {
+                xApp.Run("Workbook_Open");
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                xSheet = (Microsoft.Office.Interop.Excel.Worksheet)xBook.ActiveSheet;
+                xSheet.PrintOut(1, 1, 1, preview);
+            }
+            finally
+            {
+                xBook.Close(false);
+                xApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(xApp);
+                xSheet = null;
+                xBook = null;
+                xApp = null;
+                GC.Collect();
+            }
         }
 
     }
